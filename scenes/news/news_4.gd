8f63b8dd1b7f662e85c9500239723c37550d6443
Alex Georgiyev <aleggggg78@gmail.com>
2026-03-01 13:19:44 -0500

    News 4 glitching picture working

--- scenes/news/news_4.gd
+++ scenes/news/news_4.gd
@@ -1,4 +1,15 @@
 extends Node2D
 
+@onready var glitch = $TextureRect2
+@onready var news = $TextureRect
+
 func _on_audio_stream_player_2d_finished() -> void:
 	get_tree().change_scene_to_file("res://scenes/days/day_4.tscn")
+
+func _ready() -> void:
+	news.visible = true
+	glitch.visible = false
+	
+func _on_timer_timeout() -> void:
+	news.visible = false
+	glitch.visible = true
